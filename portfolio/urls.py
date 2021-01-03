f654bf149f4831feb54bd4a342f31f0681151973
jatinchauhann <jtnchauhan24@gmail.com>
2021-01-03 17:32:58 +0530

    Second day: Addding images n style in the web page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
