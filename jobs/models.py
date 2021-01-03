ed3d5d7942e024d5d998e09f539a956899c4372f
jatinchauhann <jtnchauhan24@gmail.com>
2021-01-03 18:57:36 +0530

    Day Three: Final

--- jobs/models.py
+++ jobs/models.py
@@ -8,3 +8,6 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     # Summary
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,5 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', jobs.views.home, name='home'),
+    path('jobs/<int:job_id>', jobs.views.detail, name='detail')
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
